1d92d1f8f2ee30b1cf1b9f0a42b82762f89929f7
Gergerov <gergerov94@gmail.com>
2022-02-08 21:09:11 +0300

    3
    3

--- CMTFMXProject.dpr
+++ CMTFMXProject.dpr
@@ -3,15 +3,19 @@ program CMTFMXProject;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  main in 'main.pas' {Form1},
+  main in 'main.pas' {FormMain},
   mInfoAboutForm in 'mInfoAboutForm.pas',
   uFamalyTree in '..\UNITS\uFamalyTree.pas',
-  uTreeViewBuilder in '..\UNITS\uTreeViewBuilder.pas';
+  uTreeViewBuilder in '..\UNITS\uTreeViewBuilder.pas',
+  uADOConnectionSetup in '..\UNITS\uADOConnectionSetup.pas',
+  uConnectionForm in 'forms\uConnectionForm.pas' {FormConnection},
+  uCustomUser in '..\UNITS\uCustomUser.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TFormMain, FormMain);
   Application.Run;
+
 end.
--- mInfoAboutForm.pas
+++ mInfoAboutForm.pas
@@ -1,87 +1,81 @@
 unit mInfoAboutForm;
 
 interface
+
 uses
   Classes,
   System.SysUtils,
-  FMX.Forms, FMX.Memo, FMX.Types, FMX.Edit
-  ;
+  FMX.Forms, FMX.Memo, FMX.Types, FMX.Edit;
 
 procedure get_parent_class_info(cls: TClass; strings: TStrings; lvl: integer);
 procedure draw_info(cls: TClass);
 function get_class_info(cls: TClass): string;
 
-implementation uses TypInfo;
+implementation
+
+uses TypInfo;
 
 procedure get_parent_class_info(cls: TClass; strings: TStrings; lvl: integer);
-  var
-    ClassTypeInfo: PTypeInfo;
-    ClassTypeData: PTypeData;
-  begin
-    if cls = TObject then
-      exit;
-    lvl := lvl + 1;
-    ClassTypeInfo := cls.ClassParent.ClassInfo;
-    ClassTypeData := GetTypeData(ClassTypeInfo);
+var
+  ClassTypeInfo: PTypeInfo;
+  ClassTypeData: PTypeData;
+begin
+  if cls = TObject then
+    exit;
+  lvl := lvl + 1;
+  ClassTypeInfo := cls.ClassParent.ClassInfo;
+  ClassTypeData := GetTypeData(ClassTypeInfo);
 
-    strings.Append(
-      IntToStr(lvl)
-      + ' '
-      + cls.ClassParent.ClassName
-      + ' '
-      + '('
-      + Format('%s', [ClassTypeData.UnitName])
-      + ')'
-    );
-    get_parent_class_info(cls.ClassParent, strings, lvl);
-  end;
+  strings.Append(IntToStr(lvl) + ' ' + cls.ClassParent.ClassName + ' ' + '(' +
+    Format('%s', [ClassTypeData.UnitName]) + ')');
+  get_parent_class_info(cls.ClassParent, strings, lvl);
+end;
 
 function get_class_info(cls: TClass): string;
-  var
-    ClassTypeInfo: PTypeInfo;
-    ClassTypeData: PTypeData;
-    str: string;
-  begin
-    ClassTypeInfo := cls.ClassInfo;
-    ClassTypeData := GetTypeData(ClassTypeInfo);
-    str := str + cls.ClassName;
-    str := str + ' ';
-    str := str + '(';
-    str := str + Format('%s', [ClassTypeData.UnitName]);
-    str := str + ')';
-    result := str;
-  end;
+var
+  ClassTypeInfo: PTypeInfo;
+  ClassTypeData: PTypeData;
+  str: string;
+begin
+  ClassTypeInfo := cls.ClassInfo;
+  ClassTypeData := GetTypeData(ClassTypeInfo);
+  str := str + cls.ClassName;
+  str := str + ' ';
+  str := str + '(';
+  str := str + Format('%s', [ClassTypeData.UnitName]);
+  str := str + ')';
+  result := str;
+end;
 
 procedure draw_info(cls: TClass);
-  var
-    frm: TForm;
-    memo: TMemo;
-    edit: TEdit;
+var
+  frm: TForm;
+  Memo: TMemo;
+  Edit: TEdit;
 
-  begin
+begin
 
-    frm := TForm.CreateNew(NIL);
+  frm := TForm.CreateNew(NIL);
 
-    frm.Width := 450;
-    frm.Height := 450;
-    frm.left := (Screen.WorkAreaWidth - frm.Width) div 2;
-    frm.top := (Screen.WorkAreaHeight - frm.Height) div 2;
+  frm.Width := 450;
+  frm.Height := 450;
+  frm.left := (Screen.WorkAreaWidth - frm.Width) div 2;
+  frm.top := (Screen.WorkAreaHeight - frm.Height) div 2;
 
-    edit := TEdit.Create(frm);
-    edit.parent := frm;
-    edit.Align := TAlignLayout.Top;
-    edit.Text := get_class_info(cls);
-    edit.ReadOnly := True;
+  Edit := TEdit.Create(frm);
+  Edit.parent := frm;
+  Edit.Align := TAlignLayout.top;
+  Edit.Text := get_class_info(cls);
+  Edit.ReadOnly := True;
 
-    memo := TMemo.Create(frm);
-    memo.parent := frm;
-    memo.Align := TAlignLayout.Client;
-    get_parent_class_info(cls, memo.Lines, 0);
-    memo.ReadOnly := True;
+  Memo := TMemo.Create(frm);
+  Memo.parent := frm;
+  Memo.Align := TAlignLayout.Client;
+  get_parent_class_info(cls, Memo.Lines, 0);
+  Memo.ReadOnly := True;
 
-    frm.ShowModal;
+  frm.ShowModal;
 
-  end;
+end;
 
 end.
-
