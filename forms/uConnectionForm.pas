4e0835a514da0bce47dd322ea6e8c2c4b9efbf6a
Gergerov <gergerov94@gmail.com>
2022-02-08 19:44:13 +0300

    2
    2

---
+++ forms/uConnectionForm.pas
@@ -0,0 +1,108 @@
+﻿unit uConnectionForm;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Edit, FMX.ListBox,
+
+  uADOConnectionSetup
+  ;
+
+type
+  TFormConnection = class(TForm)
+    panelLeft: TPanel;
+    btnConnect: TButton;
+    panelLeftTop: TPanel;
+    gbIniFilePath: TGroupBox;
+    labelIniFilePath: TLabel;
+    editIniFilePath: TEdit;
+    panelClient: TPanel;
+    gbData: TGroupBox;
+    labelConnectionSection: TLabel;
+    btnReadIniFile: TButton;
+    cbIniSection: TComboBox;
+    labelUsername: TLabel;
+    editUsername: TEdit;
+    editPassword: TEdit;
+    labelPassword: TLabel;
+    OpenDialogIniFilePath: TOpenDialog;
+    btnSearch: TButton;
+    procedure FormCreate(Sender: TObject);
+    procedure btnSearchClick(Sender: TObject);
+    procedure btnReadIniFileClick(Sender: TObject);
+    procedure disableGBData;
+    procedure activateGBData;
+    procedure btnConnectClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    ConnectionSetup: TADOConnectionSetup;
+    ConnectionString: string;
+    { Public declarations }
+  end;
+
+var
+  FormConnection: TFormConnection;
+
+
+implementation
+
+{$R *.fmx}
+
+procedure TFormConnection.btnConnectClick(Sender: TObject);
+  begin
+    ConnectionString := ConnectionSetup.GetConnectionString(cbIniSection.Selected.Text);
+    ModalResult := mrOk;
+  end;
+
+procedure TFormConnection.btnReadIniFileClick(Sender: TObject);
+  begin
+    ConnectionSetup := TADOConnectionSetup.Create('MSSQL', editIniFilePath.Text);
+    ConnectionSetup.SetSectionSelector(cbIniSection.Items);
+    if cbIniSection.Count > 0 then
+      begin
+        cbIniSection.ItemIndex := 0;
+        activateGBData;
+        exit;
+      end;
+    ShowMessage('Нет данных для подключения');
+  end;
+
+procedure TFormConnection.btnSearchClick(Sender: TObject);
+  begin
+    if OpenDialogIniFilePath.Execute then
+      begin
+        editIniFilePath.Text := OpenDialogIniFilePath.FileName;
+      end;
+  end;
+
+procedure TFormConnection.FormCreate(Sender: TObject);
+  begin
+    Width := 450;
+    Height := 450;
+    left := (Screen.WorkAreaWidth - Width) div 2;
+    top := (Screen.WorkAreaHeight - Height) div 2;
+    editIniFilePath.Text := '.\connection.ini';
+    disableGBData;
+  end;
+
+procedure TFormConnection.activateGBData;
+  begin
+    editUsername.Enabled := True;
+    editPassword.Enabled := True;
+    cbIniSection.Enabled := True;
+    btnConnect.Enabled := True;
+  end;
+
+procedure TFormConnection.disableGBData;
+  begin
+    editUsername.Enabled := False;
+    editPassword.Enabled := False;
+    cbIniSection.Enabled := False;
+    btnConnect.Enabled := False;
+  end;
+
+end.
--- main.pas
+++ main.pas
@@ -4,8 +4,8 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes,
-  System.Variants,
-  System.Rtti, System.Bindings.Outputs,
+  System.Variants, System.Rtti, System.Bindings.Outputs,
+  System.Hash,
 
   IniFiles,
 
@@ -14,27 +14,28 @@ uses
   FMX.StdCtrls, FMX.Grid, FMX.Bind.DBEngExt, FMX.Bind.Editors,
   FMX.Bind.Grid, FMX.Grid.Style,
 
-  uFamalyTree, uTreeViewBuilder, uADOConnectionSetup,
+  uFamalyTree, uTreeViewBuilder, uADOConnectionSetup, uConnectionForm,
+  uCustomUser,
 
   Data.FMTBcd, Data.DB, Data.SqlExpr, FMX.Layouts, FMX.TreeView,
 
   Data.Bind.EngExt, Data.Bind.Components, Data.Bind.Grid,
   Data.Bind.DBScope,
-  Data.Win.ADODB, FMX.ListBox;
+  Data.Win.ADODB, FMX.ListBox, FMX.Edit;
 
 type
-  TForm1 = class(TForm)
+  TFormMain = class(TForm)
     TreeView1: TTreeView;
     CTMConnection: TADOConnection;
     panelTop: TPanel;
-    btnConnect: TButton;
-    ComboBox1: TComboBox;
-    btnSetSectionSelector: TButton;
-    btnShowInfo: TButton;
-    procedure btnConnectClick(Sender: TObject);
-    procedure btnSetSectionSelectorClick(Sender: TObject);
-    procedure btnShowInfoClick(Sender: TObject);
-
+    btnConnectionForm: TButton;
+    btnDisconnect: TButton;
+    labelStatusDBTime: TEdit;
+    panelBot: TPanel;
+    labelStatusUsername: TEdit;
+    procedure btnConnectionFormClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure btnDisconnectClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -42,44 +43,101 @@ type
   end;
 
 var
-  Form1: TForm1;
+  FormMain: TFormMain;
   ConnectionIniFileName: string = '.\connection.ini';
   Conset: TADOConnectionSetup;
-  test: TStringList;
+  CTMUser: TCTMUser;
 
 implementation
 
 {$R *.fmx}
 
-procedure TForm1.btnConnectClick(Sender: TObject);
-var
-  IniFile: TIniFile;
-  Provider: string;
-  Sections: string;
-  values: string;
-  q: TADOQuery;
-
-begin
-
-    CTMConnection.ConnectionString := conset.GetConnectionString(ComboBox1.Selected.Text);
-    CTMConnection.Connected := True;
-    q := TADOQuery.Create(NIL);
-    q.Connection := CTMConnection;
-    q.SQL.Add('select getdate() as date');
-    q.Open;
-    ShowMessage(VarToStr(q.FieldValues['date']));
-end;
-
-procedure TForm1.btnSetSectionSelectorClick(Sender: TObject);
-
+procedure TFormMain.btnConnectionFormClick(Sender: TObject);
+  var
+    frm: TFormConnection;
+    q: TADOQuery;
   begin
-    conset :=  TADOConnectionSetup.Create('MSSQL', ConnectionIniFileName);
-    conset.SetSectionSelector(ComboBox1.items);
+    frm := TFormConnection.Create(NIL);
+    frm.btnReadIniFile.OnClick(frm.btnReadIniFile);
+    frm.editUsername.Text := 'CTM';
+    frm.editPassword.Text := 'CTM';
+    frm.panelLeft.Visible := False;
+    frm.ShowModal;
+
+    if frm.ModalResult = mrOk then
+      begin
+        // проверка подключения
+        try
+          CTMConnection.ConnectionString := frm.ConnectionString;
+          q := TADOQuery.Create(NIL);
+          q.Connection := CTMConnection;
+          q.SQL.Add('select getdate() as date');
+          q.Open;
+          labelStatusDBTime.Text :=
+            'Тест подключения (время): '
+            + VarToStr(q.FieldValues['date'])
+        except
+          labelStatusDBTime.Text := 'Тест подключения (время): Не подключено';
+          exit;
+        end;
+
+        try
+            begin
+              CTMUser := TCTMUser.Create(
+                frm.editUsername.Text,
+                frm.editPassword.Text,
+                CTMConnection
+              );
+            end;
+          CTMUser.Auth;
+          if not CTMUser.FisAUTH then
+            begin
+              labelStatusUsername.Text := 'Неверное имя пользователя или пароль';
+              exit;
+            end;
+          labelStatusUsername.Text :=
+            'Имя пользователя: '
+            + CTMUser.FUSERNAME
+            + '('
+            + IntToStr(CTMUser.FID)
+            + ')'
+            ;
+          btnConnectionForm.Enabled := False;
+          btnDisconnect.Enabled := True;
+        except
+          ShowMessage('Авторизация не пройдена');
+          labelStatusUsername.Text := 'Авторизация не пройдена';
+          exit;
+        end;
+      end;
+    frm.Free;
   end;
 
-procedure TForm1.btnShowInfoClick(Sender: TObject);
+procedure TFormMain.btnDisconnectClick(Sender: TObject);
   begin
-    Draw_Info(self.ClassType);
+    CTMUser.Destroy;
+    btnConnectionForm.Enabled := True;
+    btnDisconnect.Enabled := False;
+    CTMConnection.Connected := False;
+    CTMConnection.ConnectionString := '';
+    labelStatusDBTime.Text :=
+      'Disconnected at: '
+      + DateToStr(Now()) + ' '
+      + TimeToStr(Now());
+    labelStatusUsername.Text :=
+      'Disconnected at: '
+      + DateToStr(Now()) + ' '
+      + TimeToStr(Now());
+  end;
+
+procedure TFormMain.FormCreate(Sender: TObject);
+  begin
+    btnConnectionForm.OnClick(btnConnectionForm);
+
+    Width := 1200;
+    Height := 800;
+    left := (Screen.WorkAreaWidth - Width) div 2;
+    top := (Screen.WorkAreaHeight - Height) div 2;
   end;
 
 end.
